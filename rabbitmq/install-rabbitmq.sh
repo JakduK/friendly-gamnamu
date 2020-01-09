7c42e79e4df526f3751b2dbcf0072648bea9c00e
pyohwan <phjang1983@daum.net>
2020-01-10 00:39:16 +0900

    JakduK/jakduk-api#366 EPEL, OS Limits 설

--- rabbitmq/install-rabbitmq.sh
+++ rabbitmq/install-rabbitmq.sh
@@ -3,6 +3,9 @@
 # 스크립트 얻는법 : wget https://raw.githubusercontent.com/JakduK/friendly-gamnamu/master/rabbitmq/install-rabbitmq.sh
 # 공식 문서 참고 https://www.rabbitmq.com/install-rpm.html
 
+# Setup Extra Packages for Enterprise Linux (EPEL)
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
 # Install Erlang
 # Erlang package from the EPEL Repository
 yum -y install erlang
@@ -11,4 +14,10 @@ yum -y install erlang
 # Using Bintray Yum Repository
 rpm --import https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc
 wget https://raw.githubusercontent.com/JakduK/friendly-gamnamu/master/rabbitmq/rabbitmq.repo -P /etc/yum.repos.d/
-yum install rabbitmq-server
+yum -y install rabbitmq-server
+
+# To start the daemon by default when the system boots, as an administrator run
+systemctl enable rabbitmq-server.service
+
+# Setup OS limits
+wget https://raw.githubusercontent.com/JakduK/friendly-gamnamu/master/rabbitmq/limits.conf -P /etc/systemd/system/rabbitmq-server.service.d/
