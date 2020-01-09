95a32534cb6e4cb27b465c28c8efe67303ecbd58
pyohwan <phjang1983@daum.net>
2020-01-10 00:24:38 +0900

    JakduK/jakduk-api#366 RabbitMQ 설치 초안

--- rabbitmq/install-rabbitmq.sh
+++ rabbitmq/install-rabbitmq.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # CentOS 8 에서 RabbitMQ 설치를 위한 스크립트
+# 스크립트 얻는법 : wget https://raw.githubusercontent.com/JakduK/friendly-gamnamu/master/rabbitmq/install-rabbitmq.sh
 # 공식 문서 참고 https://www.rabbitmq.com/install-rpm.html
 
 # Install Erlang
@@ -9,4 +10,5 @@ yum -y install erlang
 # Install RabbitMQ Server
 # Using Bintray Yum Repository
 rpm --import https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc
-c
+wget https://raw.githubusercontent.com/JakduK/friendly-gamnamu/master/rabbitmq/rabbitmq.repo -P /etc/yum.repos.d/
+yum install rabbitmq-server
