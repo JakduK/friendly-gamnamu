4d7becae36a8a7d5d93d581637b1ce51c5d41854
pyohwan <phjang1983@daum.net>
2020-02-04 23:47:35 +0900

    certbot automatic renewal

--- nginx/install-certbot.sh
+++ nginx/install-certbot.sh
@@ -13,4 +13,8 @@ chown root /usr/local/bin/certbot-auto
 chmod 0755 /usr/local/bin/certbot-auto
 
 echo "* Get a certificate and have Certbot edit your Nginx configuration automatically *"
+echo "* 각 단계에서 직접 입력 해야 함 *"
 /usr/local/bin/certbot-auto --nginx -d jakduk.com -d api.jakduk.com -d mq.jakduk.com -d jenkins.jakduk.com -d dev-api.jakduk.com -d dev-web.jakduk.com -d www.jakduk.com -d wiki.jakduk.com
+
+echo "* Set up automatic renewal *"
+echo "0 0,12 * * * root python3 -c 'import random; import time; time.sleep(random.random() * 3600)' && /usr/local/bin/certbot-auto renew" | sudo tee -a /etc/crontab > /dev/null
