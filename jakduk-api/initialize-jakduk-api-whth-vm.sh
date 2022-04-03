9b0f998b198e94538f6d669f7b906d9cae415016
Jang,Pyohwan <pyohwan.jang@navercorp.com>
2022-04-03 17:19:05 +0900

    디렉터리, mount, systemd 초기화 추가

--- jakduk-api/initialize-jakduk-api-whth-vm.sh
+++ jakduk-api/initialize-jakduk-api-whth-vm.sh
@@ -6,7 +6,7 @@ echo "* Add jakduk user *"
 if ! grep -q "jakduk" /etc/passwd; then
 	sudo useradd jakduk
 else
-    echo "** jakduk user already exists /etc/passwd **"
+    echo "WARN : jakduk user already exists /etc/passwd"
 fi
 
 echo "* Setup sudoers for jakduk user *"
@@ -16,11 +16,10 @@ if [ ! -f $SUDOERS_DIR/jakduk-user ]; then
   wget https://raw.githubusercontent.com/JakduK/friendly-gamnamu/master/jakduk-api/jakduk-user -P $SUDOERS_DIR/
   chmod 0400 $SUDOERS_DIR/jakduk-user
 else
-	echo "! $SUDOERS_DIR/jakduk-user already exists !"
+	echo "WARN : $SUDOERS_DIR/jakduk-user already exists"
 fi
 
 echo "* Setup jenkins jakduk user id_rsa.pub *"
-SUDOERS_DIR=/etc/sudoers.d
 
 sudo /sbin/runuser -l jakduk -c "mkdir -p .ssh"
 sudo /sbin/runuser -l jakduk -c "chmod 700 .ssh"
@@ -31,5 +30,45 @@ if ! grep -q "jakduk" .ssh/authorized_keys; then
 	sudo /sbin/runuser -l jakduk -c "chmod 600 .ssh/authorized_keys"
 	sudo /sbin/runuser -l jakduk -c "rm -f jenkins_jakduk_id_rsa.pub"
 else
-    echo "** jakduk user id_rsa.pub already exists .ssh/authorized_keys **"
+    echo "WARN : jakduk user id_rsa.pub already exists .ssh/authorized_keys"
+fi
+
+echo "* Setup jakduk working directory *"
+if [ ! -d /jakduk ]; then
+  mkdir /jakduk
+  mkdir /jakduk/api
+  mkdir /jakduk/storage
+  chown -R jakduk:jakduk /jakduk
+else
+	echo "WARN : /jakduk directory already exists"
+fi
+
+echo "* Setup jakduk working directory *"
+if [ ! -d /jakduk ]; then
+  mkdir /jakduk
+  mkdir /jakduk/api
+  mkdir /jakduk/storage
+  chown -R jakduk:jakduk /jakduk
+else
+	echo "WARN : /jakduk directory already exists"
+fi
+
+echo "* Setup jakduk stoarge mount *"
+yum install -y nfs-utils
+
+if ! grep -q "jakduk/storage" /etc/fstab ; then
+  sudo /sbin/runuser -l root -c "echo '192.168.0.9:/storage /jakduk/storage nfs defaults 0 0' >> /etc/fstab"
+  sudo /sbin/runuser -l root -c "mount -a"
+else
+    echo "WARN : jakduk stoarge mount already exists /etc/fstab"
+fi
+
+echo "* Setup jakduk-api service for systemd *"
+SYSTEMD_DIR=/etc/systemd/system
+
+if [ ! -f $SYSTEMD_DIR/jakduk-api.service ]; then
+  wget https://raw.githubusercontent.com/JakduK/friendly-gamnamu/master/jakduk-api/jakduk-api.service -P $SYSTEMD_DIR/
+  systemctl enable jakduk-api.service
+else
+	echo "WARN : $SYSTEMD_DIR/jakduk-api.service already exists"
 fi
