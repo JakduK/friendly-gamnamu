553268eb6560128d5c1e81f514d06b91bfadccda
Jang,Pyohwan <pyohwan.jang@navercorp.com>
2022-05-27 20:39:16 +0900

    easy-rsa 3.1.0 버전

--- open-vpn/install-open-vpn.sh
+++ open-vpn/install-open-vpn.sh
@@ -4,6 +4,7 @@
 # 문서 참고
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-and-configure-an-openvpn-server-on-centos-7
 # https://phoenixnap.com/kb/openvpn-centos
+# https://github.com/OpenVPN/easy-rsa/
 
 echo "* Update the CentOS repositories and packages. *"
 yum update -y
@@ -18,10 +19,10 @@ echo "* Install OpenVPN. *"
 yum install -y openvpn
 
 echo "* Download Easy RSA latest version. *"
-wget https://github.com/OpenVPN/easy-rsa/archive/v3.0.8.tar.gz
-tar -xf v3.0.8.tar.gz
-rm -f v3.0.8.tar.gz
-mv easy-rsa-3.0.8 /etc/openvpn/easy-rsa
+wget https://github.com/OpenVPN/easy-rsa/archive/v3.1.0.tar.gz
+tar -xf v3.1.0.tar.gz
+rm -f v3.1.0.tar.gz
+mv easy-rsa-3.1.0 /etc/openvpn/easy-rsa
 
 echo "* Setup server.conf file. *"
 OPEN_VPN_CONF_DIR=/etc/openvpn
@@ -43,19 +44,21 @@ echo "* Create key for HMAC firewall *"
 openvpn --genkey --secret /etc/openvpn/myvpn.tlsauth
 
 echo "* Building the certificate authority. *"
+cd /etc/openvpn/easy-rsa/easyrsa3
 # CA 는 직접 입력 필요 e.g. openvpn.jakduk.dev
-/etc/openvpn/easy-rsa/easyrsa3/easyrsa build-ca nopass
+./easyrsa build-ca nopass
 
 echo "* Create a key and certificate for the server. *"
 # PEM pass phrase는 직접 입력 필요
-/etc/openvpn/easy-rsa/easyrsa3/easyrsa build-server-full server
+./easyrsa build-server-full server
 
 echo "* Generate a Diffie-Hellman key exchange file. *"
-/etc/openvpn/easy-rsa/easyrsa3/easyrsa gen-dh
+./easyrsa gen-dh
 
 echo "* Create a certificate and key for client1. *"
+# /etc/openvpn/easy-rsa/easyrsa3 디렉터리에서 실행해야 함
 # PEM pass phrase는 직접 입력 필요
-/etc/openvpn/easy-rsa/easyrsa3/easyrsa build-client-full client1
+./easyrsa build-client-full client1
 
 echo "* Copy key and certificate files to /etc/openvpn *"
 cp /etc/openvpn/easy-rsa/easyrsa3/pki/ca.crt /etc/openvpn/easy-rsa/easyrsa3/pki/dh.pem /etc/openvpn
@@ -65,5 +68,11 @@ cp /etc/openvpn/easy-rsa/easyrsa3/pki/private/ca.key /etc/openvpn/easy-rsa/easyr
 echo "* Modify Firewall *"
 firewall-cmd --zone=public --add-service openvpn
 firewall-cmd --zone=public --add-service openvpn --permanent
-firewall-cmd --add-masquerade
-firewall-cmd --add-masquerade --permanent
+#firewall-cmd --add-masquerade
+#firewall-cmd --add-masquerade --permanent
+
+echo "* Enable OpenVPN *"
+systemctl start openvpn@server.service
+# systemd-tty-ask-password-agent 를 실행해서 비밀번호를 넣어야 한다.
+systemctl enable openvpn@server.service
+
